ec0da7c4a7f1050c1f4dbcf8829c036ea2874bab
Venku Buragadda <venku.buragadda.acc@gmail.com>
2023-05-11 07:18:32 -0500

    scopes and rearranging the code

--- py_data_model/list_comp_and_generator_exp.py
+++ py_data_model/list_comp_and_generator_exp.py
@@ -2,21 +2,14 @@ import array
 
 symbols = '$¢£¥€¤'
 global last
-
+last = 100
 def scope_of_a_variable_in_list_gen_comprehensions():
-    # codes = [65, 66, 67]
     
-    x = 'ABC'
 
-    # you can use x as a local variable in list comp
-    val = [ord(x) for x in x]
-    print(F"Using x as a local variable and it will still hold orginal: {x} and final results : {val}")
 
-    # This Walrus operator is availble from Py 3.8 version
-    code = [last := ord(c) for c in x]
-    print(F"You will see the last is still has the last assinged values: {last}")
 
-if __name__ == "__main__":
+    # List comprehension
+
     li_comp = [ord(c) for c in symbols]
     print(li_comp)
 
@@ -33,13 +26,40 @@ if __name__ == "__main__":
     
 
 
+
     # generator functions
 
     t = tuple(ord(c) for c in symbols)
     print(f"{t} - values of tules from a generator function")
 
-
+    # it is mandatory to have () in the generator funtion for array.array 
+    # since array.array takes two arguments.
     ar = array.array("I", (ord(c) for c in symbols))
     print(F"{ar} value of array.array generator function")
+
+    
+    # Scope of a variable inside a Gen or List comp
+    
+    global last
+    print(f"Last after decl {last}")
+    last = 10
+    print(f"Last after assignment {last}")
+    
+    x = 'ABC'
+
+    # you can use x as a local variable in list comp
+    val = [ord(x) for x in x]
+    print(F"Using x as a local variable and it will still hold orginal: {x} and final results : {val}")
+
+    # This Walrus operator is availble from Py 3.8 version
+    code = [last := ord(c) for c in x]
+    print(F"Local variable 'last' still has the final assinged values: {last}")
+
+if __name__ == "__main__":
+ 
+    
+
+
+    
 
     scope_of_a_variable_in_list_gen_comprehensions()
